ddfe1d786ef318982d50f3e68a751b70cdbbef87
Vladimir Farrera <vfv9312@gmail.com>
2023-03-29 01:28:52 -0600

    agregamos los productos de forma dinamica con javascript

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const movilmenu = document.querySelector('.mobile-menu');
 const menuicono = document.querySelector('.menu11');
 const compras = document.querySelector('.product-detail');
 const iconocompras = document.querySelector('.navbar-shopping-cart');
+const cardsContainer=document.querySelector('.cards-container');
 
 
 menuicono.addEventListener('click', togglemovilmenu);
@@ -37,4 +38,59 @@ function mostrarcarrito() {
     compras.classList.toggle('inactivo')// activa la pestaña movil
     
 }
+
+const listaProductos = [];
+
+listaProductos.push ({
+    nombre: 'Motocicleta',
+    precio: 40000,
+    imagen: "https://images.pexels.com/photos/2393816/pexels-photo-2393816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+});
+
+listaProductos.push ({
+    nombre: 'Bicicleta',
+    precio: 4000,
+    imagen: "https://bicimaya.com/wp-content/uploads/2021/05/A-scaled.jpg",
+});
+
+listaProductos.push ({
+    nombre: 'Patineta',
+    precio: 1000,
+    imagen: "https://m.media-amazon.com/images/I/61yMreL1JqL._AC_SX425_.jpg",
+});
+
+
+function renderProducts(arr){
+    for(product of arr){
+        const productCard=document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg=document.createElement('img');
+        productImg.setAttribute('src',product.imagen);
+
+        const productInfo=document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv=document.createElement('div');
+
+        const productPrice=document.createElement('p');
+        productPrice.innerText='$'+product.precio;
+        const productName=document.createElement('p');
+        productName.innerText=product.nombre;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure=document.createElement('figure');
+        const productImgCart=document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+        
+        productInfoFigure.appendChild(productImgCart);
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        cardsContainer.appendChild(productCard);}}
+
+renderProducts(listaProductos);
 
