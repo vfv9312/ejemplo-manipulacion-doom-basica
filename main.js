58d6aff5d13becee2a17ed7e4f51433f2bd2bbc3
Vladimir Farrera <vfv9312@gmail.com>
2023-03-29 00:27:36 -0600

    agregamos pestañas en el index de menu, compras y se edita js para que no se abran pestañas al mismo tiempo

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const escritoriokenu = document.querySelector('.desktop-menu');
 const movilmenu = document.querySelector('.mobile-menu');
-const menuicono = document.querySelector('.menu11')
+const menuicono = document.querySelector('.menu11');
+const compras = document.querySelector('.product-detail');
+const iconocompras = document.querySelector('.navbar-shopping-cart');
+
 
 menuicono.addEventListener('click', togglemovilmenu);
 menuEmail.addEventListener('click', mostrarmenu);
+iconocompras.addEventListener('click', mostrarcarrito);
+
 
 
 function mostrarmenu() {
@@ -12,5 +17,24 @@ function mostrarmenu() {
 }
 
 function togglemovilmenu() {
-    movilmenu.classList.toggle('inactivo')
+    const isAsideClosed = compras.classList.contains('inactivo'); //agarra el valor inactivo, si esta inactivo la pestaña de compras
+  
+    if (!isAsideClosed) { //si esta activo la pestaña de compras entra esta condicion
+        compras.classList.add('inactivo');  //desactivo la pestaña de compras
+        //como no hay retun continua leyendo la funcion
+    }
+    
+    movilmenu.classList.toggle('inactivo'); // activa la pestaña movil
 }
+
+function mostrarcarrito() {
+    const isMobileMenuClosed = movilmenu.classList.contains('inactivo'); //agarra el valor inactivo, si esta inactivo la pestaña de movil
+
+    if (!isMobileMenuClosed) {//si esta activo la pestaña de menu entra esta condicion
+        movilmenu.classList.add('inactivo'); //desactivo la pestaña de menu
+      }
+
+    compras.classList.toggle('inactivo')// activa la pestaña movil
+    
+}
+
