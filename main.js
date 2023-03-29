4f2b7d66a5fb4120c0bc0e66eec7b4e9e3eb5282
Vladimir Farrera <vfv9312@gmail.com>
2023-03-28 18:04:50 -0600

    agregamos

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
-const escritoriokenu = document.querySelector('.desktop-menu')
+const escritoriokenu = document.querySelector('.desktop-menu');
+const movilmenu = document.querySelector('.mobile-menu');
+const menuicono = document.querySelector('.menu11')
+
+menuicono.addEventListener('click', togglemovilmenu);
 menuEmail.addEventListener('click', mostrarmenu);
+
 
 function mostrarmenu() {
     escritoriokenu.classList.toggle('inactivo');// quita o pone lo que coloquemos
+}
+
+function togglemovilmenu() {
+    movilmenu.classList.toggle('inactivo')
 }
